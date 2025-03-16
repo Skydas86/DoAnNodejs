168b72b59b4bd53a48c0fdaf67b35b429c6f9d64
UocDuocTTTMA <ngtrkien1105@gmai.com>
2025-03-17 00:31:00 +0700

    new login page

--- src/router/web.js
+++ src/router/web.js
@@ -4,16 +4,17 @@ const express = require('express');
 const router = express.Router();
 
 //router.Method("/route",handler)
-router.get('/', (req, res) => {
-  res.render('home.ejs')
-});
+
 router.get('/demo2', ABC );
 router.get('/demo', (req, res) => {
     res.render('sample.ejs')
   });
   
-  router.get('/homepage', (req, res) => {
+  router.get('/', (req, res) => {
     res.render('homepage.ejs')
+  });
+  router.get('/login', (req, res) => {
+    res.render('login.ejs')
   });
 
 module.exports = router;
